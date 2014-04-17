d1b74b84623afa2e983f7a2b2b924837a6cb2d53
Nikita Zdanovitch <3hb.tch@gmail.com>
2014-04-16 21:01:33 -0300

    se agregaron funciones para interactuar con libros, que emocion, wiii

--- bookshelf.hs
+++ bookshelf.hs
@@ -1,6 +1,7 @@
 type BookName = String
 type BookId = Int
-type BookAuthors = [String]
+type BookAuthor = String
+type BookAuthors = [BookAuthor]
 
 --data BookInfo = Book BookId BookName BookAuthors
 --			  | AnonimousBook BookId BookName
@@ -19,5 +20,27 @@ data BookInfo = Book {
 				bookName :: BookName }
 			deriving(Eq, Show)
 
-
+--weird
+sameId :: BookInfo -> BookInfo -> Bool
 sameId (Book id1 _ _) (Book id2 _ _) = id1 == id2
+sameId (Book id1 _ _) (AnonymousBook id2 _) = id1 == id2
+sameId (AnonymousBook id1 _) (Book id2 _ _) = id1 == id2
+sameId (AnonymousBook id1 _) (AnonymousBook id2 _) = id1 == id2
+
+--much much better
+sameId2 :: BookInfo -> BookInfo -> Bool
+sameId2 a b = bookId(a) == bookId(b)
+
+sameAuthors :: BookInfo -> BookInfo -> Bool
+sameAuthors (AnonymousBook _ _) _ = undefined
+sameAuthors _ (AnonymousBook _ _) = undefined
+sameAuthors a b = bookAuthors(a) == bookAuthors(b)
+
+hasAuthor :: BookAuthor -> BookInfo -> Bool
+hasAuthor _ (AnonymousBook _ _) = undefined
+hasAuthor author b = isIn (bookAuthors(b)) author
+
+isIn :: (Eq a) => [a] -> a -> Bool
+isIn [] _ = False
+isIn xs x | head xs == x = True
+isIn xs x | head xs /= x = isIn (tail xs) x
