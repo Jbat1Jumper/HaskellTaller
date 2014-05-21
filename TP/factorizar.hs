4de363bd29e4117c2eeaf46ceb68b106fee8a355
leamotta <leamotta@msn.com>
2014-05-21 18:31:05 -0300

    Update factorizar.hs
    Aeroventilas

--- TP/factorizar.hs
+++ TP/factorizar.hs
@@ -17,11 +17,14 @@ son_coprimos_a q (x:xs) = if not (es_coprimo q x)
 dos_a_n :: Integer -> [Integer]
 dos_a_n n = [2..n]
 
+es_divisible_por :: Integer -> Integer -> Bool
+es_divisible_por x y = veces_divisible_por y x >  0
+
 dos_a_mitad :: Integer -> [Integer]
 dos_a_mitad n = dos_a_n (floor (n % 2))
 
-posibles_factores :: Integer -> [Integer]
-posibles_factores n = filter es_primo (dos_a_mitad n)
+factores :: Integer -> [Integer]
+factores n = filter es_primo (filter (es_divisible_por n) (dos_a_mitad n))
 
 es_primo :: Integer -> Bool
 es_primo 1 = False
@@ -33,14 +36,11 @@ veces_divisible_por p n = if (mod n p) == 0
 					      then 1 + veces_divisible_por p (floor(n % p))
 					      else 0
 
-factor :: Integer -> Integer -> (Integer, Integer)
-factor n p = (p, veces_divisible_por p n)
-
-no_nulo :: (Integer, Integer) -> Bool
-no_nulo a = snd a /= 0
+multiplicidad :: Integer -> Integer -> (Integer, Integer)
+multiplicidad n p = (p, veces_divisible_por p n)
 
 factorizar :: Integer -> [(Integer, Integer)]
 factorizar n | n<1 = undefined
-factorizar n | es_primo n =  map (factor n) [n]
-factorizar n = filter no_nulo (map (factor n) (posibles_factores n))
+factorizar n | es_primo n =  map (multiplicidad n) [n]
+factorizar n = map (multiplicidad n) (factores n)
 
