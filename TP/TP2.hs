6d7164064b65caef8a29792e88a3a8d48eb98583
leamotta <leamotta@msn.com>
2014-06-22 14:50:04 -0300

    terminado

--- TP/TP2.hs
+++ TP/TP2.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Ratio
 
 mcd :: Integer -> Integer -> Integer
 mcd 0 b = b
@@ -51,9 +52,31 @@ poli2 = Suma (Cte (crearR 1 3)) (Prod Var (Suma (Cte (crearR 2 5)) (Prod Var Var
 poli3 = [unoR,crearRI (-2),crearRI 2,crearRI (-2),unoR]
 
 
---Ejercicio 1
---coeficientes :: Poli -> [Racional]
 
+--Ejercicio 1
+coeficientes :: Poli -> [Racional]
+coeficientes (Cte x) = [x]
+coeficientes Var = ceroR:[unoR] 
+coeficientes (Suma pol1 pol2) = sumarPoli (coeficientes pol1) (coeficientes pol2)
+coeficientes (Prod pol1 pol2) = multiplicarPoli (coeficientes pol1)  (coeficientes pol2)
+
+multiplicarPorConst :: Racional -> [Racional] -> [Racional]
+multiplicarPorConst a p1 = map (a `multR`) p1
+
+multiplicarPoli :: [Racional] -> [Racional] -> [Racional]
+multiplicarPoli [] p2 = []
+multiplicarPoli (p:p1) p2 = let pPorP2 = multiplicarPorConst p p2
+                                xPorP1Porp2 = multiplicarPorX $ multiplicarPoli p1 p2
+                      in sumarPoli pPorP2 xPorP1Porp2 
+
+multiplicarPorX :: [Racional] -> [Racional]
+multiplicarPorX p = ceroR:p
+
+sumarPoli :: [Racional] -> [Racional] -> [Racional]
+sumarPoli [] [] = []
+sumarPoli [] b = sumarPoli [ceroR] b
+sumarPoli a [] = sumarPoli a [ceroR]
+sumarPoli a b = sumaR (head a) (head b) : sumarPoli (tail a) (tail b)
 
 --Ejercicio 2
 polinomio :: [Racional] -> Poli
@@ -61,15 +84,59 @@ polinomio [R a b] = Cte (R a b)
 polinomio a = Suma (Cte (head a)) (Prod Var (polinomio (tail a)))
 
 --Ejercicio 3
---evaluar :: Poli -> Racional -> Racional
+evaluar :: Poli -> Racional -> Racional
+evaluar a b = evaluarList (coeficientes a) b
 
 evaluarList :: [Racional] -> Racional -> Racional
 evaluarList a b | length a ==1 = head a
-evaluarList a b = sumaR (multR (last a) (multev (tail a) b)) (evaluarList (init a) b)
+evaluarList a b = sumaR (multR (last a) (multPorGrado (tail a) b)) (evaluarList (init a) b)
 
-multev :: [Racional] -> Racional -> Racional
-multev a b | length a > 0 = multR b (multev (tail a) b)
-multev a b = unoR
+multPorGrado :: [Racional] -> Racional -> Racional
+multPorGrado a b | length a > 0 = multR b (multPorGrado (tail a) b)
+multPorGrado a b = unoR
+
 
 --Ejercicio 4
---raicesRacionales :: [Racional] -> [Racional]
+--Usando el teorema de la raiz racional
+raicesRacionales :: [Racional] -> [Racional]
+raicesRacionales [] = []
+raicesRacionales a | head a == ceroR = sort ( nub( ceroR : raicesRacionales (tail a))) -- caso Ter. Ind. == 0
+raicesRacionales a = sort (nub (evaluarListRaices a (multiplicarPorConst (crearR (-1) 1) posRaices)  ++ evaluarListRaices a posRaices )) where posRaices = (posiblesRaices (posiblesRaicesNum a) (posiblesRaicesDen a))
+
+
+es_divisible_por :: Integer -> Integer -> Bool
+es_divisible_por x y = veces_divisible_por y x >  0
+
+factores :: Integer -> [Integer]
+factores n | n < 0 = factores (-n)
+factores n = filter (es_divisible_por n) [1..n]
+
+veces_divisible_por :: Integer -> Integer -> Integer
+veces_divisible_por 1 n = 1
+veces_divisible_por p n = if (mod n p) == 0
+					      then 1 + veces_divisible_por p (floor(n % p))
+					      else 0
+
+mcmDiv :: [Racional] -> Racional
+mcmDiv [] = unoR
+mcmDiv a = crearR 1 (lcm (denominador (head a)) (denominador (mcmDiv (tail a))))
+
+mcmDivNum :: [Racional] -> Racional
+mcmDivNum a = crearR (denominador (mcmDiv a)) 1
+
+multPorMcm :: [Racional] -> [Racional]
+multPorMcm a = map (mcmDivNum a `multR`) a
+
+posiblesRaicesNum:: [Racional] -> [Integer]
+posiblesRaicesNum a = factores (numerador (head (multPorMcm a)))
+
+posiblesRaicesDen:: [Racional] -> [Integer]
+posiblesRaicesDen a = factores (numerador (last (multPorMcm a)))
+
+posiblesRaices:: [Integer] -> [Integer] -> [Racional]
+posiblesRaices num den =  nub ( ceroR : [crearR x y | x <- num, y<- den])
+
+evaluarListRaices :: [Racional] -> [Racional] -> [Racional]
+evaluarListRaices pol [] = []
+evaluarListRaices pol r | evaluarList pol (head r) == ceroR = head r : evaluarListRaices pol (tail r)
+evaluarListRaices pol r	=  evaluarListRaices pol (tail r)
